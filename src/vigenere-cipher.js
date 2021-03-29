116c11ccc93a177768dbd69aa9b2ee960af2b252
OlhaMatyashovska <70781130+OlhaMatyashovska@users.noreply.github.com>
2021-03-29 21:28:47 +0300

    feat: update vinegar-cipher.task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,56 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+  }
+
+  encrypt(input, key) {
+    if (!input || !key) throw new Error();
+
+    const formatted_input = input.toUpperCase().split("");
+    const formatted_key = key.toUpperCase().split("");
+
+    const result = formatted_input.map(letter => {
+      const letter_code = letter.charCodeAt(0) - 65;
+      let result = letter;
+
+      if (letter_code >= 0 && letter_code <= 25) {
+        result = String.fromCharCode(
+          ((letter_code + formatted_key[0].charCodeAt(0) - 65) % 26) + 65
+        );
+        formatted_key.push(formatted_key.shift());
+      }
+      return result;
+    });
+
+    !this.direct && result.reverse();
+
+    return result.join("");
+  }
+
+  decrypt(input, key) {
+    if (!input || !key) throw new Error();
+
+    const formatted_input = input.split("");
+    const formatted_key = key.toUpperCase().split("");
+
+    const result = formatted_input.map(letter => {
+      const letter_code = letter.charCodeAt(0) - 65;
+      let result = letter;
+
+      if (letter_code >= 0 && letter_code <= 25) {
+        result = String.fromCharCode(
+          ((letter_code - (formatted_key[0].charCodeAt(0) - 65) + 26) % 26) + 65
+        );
+        formatted_key.push(formatted_key.shift());
+      }
+      return result;
+    });
+
+    !this.direct && result.reverse();
+
+    return result.join("");
   }
 }
 
